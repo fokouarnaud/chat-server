7d520fd213deff6a62bf50f500a7b01d22f8979a
fokouarnaud <fokouarnaud@gmail.com>
2022-05-25 06:42:53 +0200

    Update Message.java

--- src/main/java/com/fokouarnaud/chatserver/model/Message.java
+++ src/main/java/com/fokouarnaud/chatserver/model/Message.java
@@ -17,4 +17,34 @@ public class Message {
     private String message;
     private String date;
     private Status status;
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 }
