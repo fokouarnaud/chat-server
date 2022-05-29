0badf337c9ea68da9743b506a32c4d351a8942fb
fokouarnaud <fokouarnaud@gmail.com>
2022-05-29 06:29:48 +0100

    Update WebSocketConfig.java

--- src/main/java/com/fokouarnaud/chatserver/config/WebSocketConfig.java
+++ src/main/java/com/fokouarnaud/chatserver/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -23,4 +24,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		config.enableSimpleBroker("/chatroom", "/user");
 		config.setUserDestinationPrefix("/user");
 	}
+	@Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(200000); // default : 64 * 1024
+        registration.setSendTimeLimit(20 * 10000); // default : 10 * 10000
+        registration.setSendBufferSizeLimit(3* 512 * 1024); // default : 512 * 1024
+
+    }
 }
