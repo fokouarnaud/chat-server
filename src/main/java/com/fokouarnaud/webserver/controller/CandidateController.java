df07882bf300ea7b2e74581dab2f58c723c18271
fokouarnaud <fokouarnaud@gmail.com>
2022-06-01 14:53:06 +0200

    Update CandidateController.java

--- src/main/java/com/fokouarnaud/webserver/controller/CandidateController.java
+++ src/main/java/com/fokouarnaud/webserver/controller/CandidateController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fokouarnaud.webserver.entity.Candidate;
@@ -36,8 +37,8 @@ public class CandidateController {
 			return service.getCandidates();
 		}
 
-		@GetMapping("/candidate/{id}")
-		public Candidate findCandidateById(@PathVariable int id) {
+		@GetMapping("/candidate")
+		public Candidate findCandidateById(@RequestParam(value="id") int id) {
 			return service.getCandidatebById(id);
 		}
 
